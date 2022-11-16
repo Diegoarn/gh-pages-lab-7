e69da6430b7c73712ecee6c9ae128fd1ab1b8cdc
Diego <arnedoav@gmail.com>
2022-11-16 16:53:56 +0100

    initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,36 @@
+const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                exportLocalsConvention: "camelCase",
+                localIdentName: "[path][name]__[local]--[hash:base64:5]",
+                localIdentContext: path.resolve(__dirname, "src"),
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+});
